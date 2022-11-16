d71482a886232d93b5a650049fc9e23deca98036
Victoria Titova <krilatixnet@yandex.ru>
2022-11-16 16:46:37 +0300

    Добавила сериалайзеры

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from reviews.models import Category, Genre, Title
+from .permissions import IsAdminOrReadOnly
+from .serializers import (
+    CategorySerializer,
+    GenreSerializer,
+    TitleSerializer
+)
+from .viewsets import CreateDeleteListViewSet
+
+
+class CategoryViewSet(CreateDeleteListViewSet):
+    queryset = Category.objects.all
+    serializer_class = CategorySerializer
+    permission_classes = IsAdminOrReadOnly
+
+
+class GenreViewSet(CreateDeleteListViewSet):
+    queryset = Genre.objects.all
+    serializer_class = GenreSerializer
+    permission_classes = IsAdminOrReadOnly
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all
+    serializer_class = TitleSerializer
+    permission_classes = IsAdminOrReadOnly
