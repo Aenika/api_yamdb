0cf9adca0e9fff282e1ad475fa9885afa1daf372
Victoria Titova <krilatixnet@yandex.ru>
2022-11-27 00:13:15 +0300

    Changed is_admin, is_moderator

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -3,21 +3,21 @@ from rest_framework import permissions
 from core.constants import ADMIN, MODERATOR
 
 
-def is_admin(self, user):
-    return user.role == ADMIN
+def is_admin(obj):
+    return obj.role == ADMIN
 
 
-def is_moderator(self, user):
-    return user.role == MODERATOR
+def is_moderator(obj):
+    return obj.role == MODERATOR
 
 
 class IsAdmin(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return self.is_admin(request.user) or request.user.is_superuser
+        return is_admin(request.user) or request.user.is_superuser
 
 
 class IsModerator(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return self.is_moderator(request.user)
+        return is_moderator(request.user)
